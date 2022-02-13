6037f31768008a15e39232f5de750f709b5e5a06
CsepJ <sepbot3240@gmail.com>
2022-02-13 23:45:52 +0900

    Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from flask import Flask
+
+app=Flask(__name__)
+@app.route("/")
+def home():
+    return '''
+    <h1>Hello user</h1>
+    '''
+
+@app.route("/function/<string:function>")
+def func(function):
+    print(function)
+    x = np.arange(0,100)
+    y = eval(function)
+    plt.plot(x,y)
+    plt.title(function)
+    plt.grid(True)
+    plt.box(True)
+    plt.subplot(1,1,1)
+    plt.savefig("plot.png", dpi=95)
+    plt.cla()
+    return "Good"
+
+if __name__ == '__main__':
+    app.run(debug=True)
