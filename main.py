999b5ec726e428e174ad8b23f138462fb3d99e6f
CsepJ <sepbot3240@gmail.com>
2022-02-14 00:43:25 +0900

    Image

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from flask import Flask
 
-app=Flask(__name__)
+app=Flask(__name__, static_url_path='/image')
 @app.route("/")
 def home():
     return '''
