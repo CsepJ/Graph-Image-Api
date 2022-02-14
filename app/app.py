d316bd83336ccd430c690a831ffacc6889f6e645
CsepJ <sepbot3240@gmail.com>
2022-02-14 12:20:26 +0900

    path

--- app/app.py
+++ app/app.py
@@ -19,6 +19,6 @@ def func(function):
     plt.grid(True)
     plt.box(True)
     plt.subplot(1,1,1)
-    plt.savefig("./image/plot.png", dpi=95)
+    plt.savefig("/image/plot.png", dpi=95)
     plt.cla()
     return "Good"
