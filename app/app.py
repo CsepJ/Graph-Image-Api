03a7f0fb85e3e094ed5cd7a0d1ee9c47396a7631
SepJ <68334508+CsepJ@users.noreply.github.com>
2022-02-14 15:35:12 +0900

    Update app.py

--- app/app.py
+++ app/app.py
@@ -15,12 +15,12 @@ def func(function):
     print(function)
     x = arange(0,100)
     try:
-        function.replace("^", "**").replace("×", "*").replace("÷", "/").replace("²","**2").replace("½", "**1/2").replace("³", "**3").replace("⁴", "**4").replace("⅓", "**1/3").replace("⅔", "**2/3").replace("¼", "**1/4").replace("¾", "**3/4")
-        y = eval(function)
+        let comFunc = function.replace("^", "**").replace("×", "*").replace("÷", "/").replace("²","**2").replace("½", "**1/2").replace("³", "**3").replace("⁴", "**4").replace("⅓", "**1/3").replace("⅔", "**2/3").replace("¼", "**1/4").replace("¾", "**3/4")
+        y = eval(comFunc)
         plt.plot(x,y)
         plt.title(function)
         plt.grid(True)
-        plt.savefig(os.path.dirname(os.path.abspath(__file__))+"/image/plot.png", dpi=95)
+        plt.savefig(os.path.dirname(os.path.abspath(__file__))+"/image/plot-"+function+".png", dpi=95)
         plt.cla()
         return "Good"
     except:
