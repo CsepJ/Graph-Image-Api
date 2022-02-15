c84d2175fe348bd71958ae5bc36111d5cf89a109
CsepJ <sepbot3240@gmail.com>
2022-02-15 23:32:32 +0900

    Saved

--- app/app.py
+++ app/app.py
@@ -11,9 +11,9 @@ def home():
     <h1>Hello user</h1>
     '''
 
-@app.route("/function/<string:function>/<string:xvalue>")
-def func(function,xvalue):
-    x = np.arange(0,100) if regexp.sub("^((?!((\d)|(x)|(math.)+(\w{1,})\(x\)|(np.)+(\w{1,})\(x\)|(np.)+(\w{1,})|(math.)+(\w{1,}))).)*$","",xvalue) == "" else regexp.sub("^((?!((\d)|(x)|(math.)+(\w{1,})\(x\)|(np.)+(\w{1,})\(x\)|(np.)+(\w{1,})|(math.)+(\w{1,}))).)*$","",xvalue)
+@app.route("/function/<string:function>")
+def func(function):
+    x = np.linspace(0,2*np.pi, num=150)
     print(x)
     try:
         comFunc = function.replace("^", "**").replace("×", "*").replace("÷", "/").replace("²","**2").replace("½", "**1/2").replace("³", "**3").replace("⁴", "**4").replace("⅓", "**1/3").replace("⅔", "**2/3").replace("¼", "**1/4").replace("¾", "**3/4")
@@ -27,12 +27,11 @@ def func(function,xvalue):
             num = "0" if regexp.sub("^((?!((\d)|(x)|(math.)+(\w{1,})\(x\)|(np.)+(\w{1,})\(x\)|(np.)+(\w{1,})|(math.)+(\w{1,}))).)*$","",word) == "" else regexp.sub("^((?!((\d)|(x)|(math.)+(\w{1,})\(x\)|(np.)+(\w{1,})\(x\)|(np.)+(\w{1,})|(math.)+(\w{1,}))).)*$","",word)
             array.insert(index,pm+num)
         array = "".join(array)
-        x=eval(x)
         y = eval(array)
         plt.plot(x,y)
         plt.title(function)
         plt.grid(True)
-        plt.savefig(os.path.dirname(os.path.abspath(__file__))+"/image/plot-"+function+"-"+xvalue+".png", dpi=95)
+        plt.savefig(os.path.dirname(os.path.abspath(__file__))+"/image/plot-"+function.replace("*", "")+".png", dpi=95)
         plt.cla()
         return "Good"
     except Exception as e:
