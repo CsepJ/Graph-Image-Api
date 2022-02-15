5c1d436412181a5212dbb943e95ec2799404d404
SepJ <68334508+CsepJ@users.noreply.github.com>
2022-02-15 16:08:03 +0900

    Secured

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
 from numpy import *
 import matplotlib.pyplot as plt
 from flask import Flask
-import os
+import math as Math
 import re as regexp
-import keyword
-
 app=Flask(__name__,static_folder="./image/")
 @app.route("/")
 def home():
@@ -18,9 +16,15 @@ def func(function):
     x = arange(0,100)
     try:
         comFunc = function.replace("^", "**").replace("×", "*").replace("÷", "/").replace("²","**2").replace("½", "**1/2").replace("³", "**3").replace("⁴", "**4").replace("⅓", "**1/3").replace("⅔", "**2/3").replace("¼", "**1/4").replace("¾", "**3/4")
-        comFunc = regexp.sub("[ㄱ-힣]", "", comFunc)
-        for word in keyword.kwlist:
-            comFunc = regexp.sub("("+word+")", "", comFunc)
+        text = regexp.sub("[ㄱ-힣]", "", comFunc)
+        array = []
+        result = regexp.split('(?:-)|(?:\+)', text)
+        resultWithpm = regexp.split("(-|\+)", text)
+        for word in result:
+            index=int(result.index(word))
+            pm = "+" if index==0 else resultWithpm[index*2-1]
+            num = "0" if regexp.sub("^((?!((\d)|(x)|(Math.)+(\w{1,})\(x\)|(Math.)+(\w{1,}))).)*$","",word) == "" else regexp.sub("^((?!((\d)|(x)|(Math.)+(\w{1,})\(x\)|(Math.)+(\w{1,}))).)*$","",word)
+            array.insert(index,pm+num)
         y = eval(comFunc)
         plt.plot(x,y)
         plt.title(function)
