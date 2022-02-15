ea0eeb1300df12fae0de7d704888b2e73daff6c4
SepJ <68334508+CsepJ@users.noreply.github.com>
2022-02-15 21:13:20 +0900

    xvalue

--- app/app.py
+++ app/app.py
@@ -11,9 +11,9 @@ def home():
     <h1>Hello user</h1>
     '''
 
-@app.route("/function/<string:function>")
-def func(function):
-    x = np.arange(0,10)
+@app.route("/function/<string:function>/<string:xvalue>")
+def func(function,x):
+    x = np.arange(0,100) if regexp.sub("^((?!((\d)|(x)|(math.)+(\w{1,})\(x\)|(np.)+(\w{1,})\(x\)|(np.)+(\w{1,})|(math.)+(\w{1,}))).)*$","",x) == "" else regexp.sub("^((?!((\d)|(x)|(math.)+(\w{1,})\(x\)|(np.)+(\w{1,})\(x\)|(np.)+(\w{1,})|(math.)+(\w{1,}))).)*$","",x)
     try:
         comFunc = function.replace("^", "**").replace("×", "*").replace("÷", "/").replace("²","**2").replace("½", "**1/2").replace("³", "**3").replace("⁴", "**4").replace("⅓", "**1/3").replace("⅔", "**2/3").replace("¼", "**1/4").replace("¾", "**3/4")
         text = regexp.sub("[ㄱ-힣]", "", comFunc)
